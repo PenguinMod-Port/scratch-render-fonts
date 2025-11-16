170d4f52478f721d6a50991d8a9a9b8a140a0c7e
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2025-11-16 00:44:06 -0700

    add pm fonts

--- src/index.js
+++ src/index.js
@@ -12,7 +12,15 @@ const getFonts = function () {
         'Marker': require('base64-loader!./Knewave.woff2'),
         'Curly': require('base64-loader!./Griffy-Regular.woff2'),
         'Pixel': require('base64-loader!./Grand9K-Pixel.woff2'),
-        'Scratch': require('base64-loader!./ScratchSavers_b2.woff2')
+        'Scratch': require('base64-loader!./ScratchSavers_b2.woff2'),
+        'Technological': require('base64-loader!./MonospaceBold.woff2'),
+        'Bubbly': require('base64-loader!./QTKooper.woff2'),
+        'Bits and Bytes': require('base64-loader!./freecam-v2.woff2'),
+        'Playful': require('base64-loader!./BadComic-Regular.woff2'),
+        'Arcade': require('base64-loader!./PressStart2P.woff2'),
+        'Archivo': require('base64-loader!./Archivo-Regular.woff2'),
+        'Archivo Black': require('base64-loader!./Archivo-Black.woff2'),
+        'Thin': require('base64-loader!./Xtraflexidisc.woff2'),
     };
     /* eslint-enable global-require */
 
